c615c07d4ec54bd9c546d97b748e713f7b1c3267
IliyaNikov <74715206+IliyaNikov@users.noreply.github.com>
2022-11-03 22:44:10 +0100

    more forgotten files

---
+++ Assets/Destroy.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "zombie") {
+            Destroy(collision.gameObject);
+        }
+    }
+}
